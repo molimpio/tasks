f28f7468d9c8d6d771911fc74d274e363379b763
Moises <moises@rcbdesenvolvimento.com.br>
2019-01-17 13:52:28 -0200

    excluindo task com Swipeable

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,12 +3,14 @@ import {
     StyleSheet,
     Text,
     View,
-    TouchableWithoutFeedback
+    TouchableWithoutFeedback,
+    TouchableOpacity
 } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import moment from 'moment'
 import 'moment/locale/pt-br'
 import commonStyles from '../commonStyles'
+import Swipeable from 'react-native-swipeable'
 
 export default props => {
     let check = null
@@ -27,20 +29,45 @@ export default props => {
     const descStyle = props.doneAt !== null ?
         {textDecorationLine: 'line-through'} : {}
 
-    return (
-        <View style={styles.container}>
-            <TouchableWithoutFeedback onPress={() => props.toggleTask(props.id)}>
-                <View style={styles.checkContainer}>{check}</View>
-            </TouchableWithoutFeedback>
-            <View>
-                <Text style={[styles.description, descStyle]}>
-                    {props.desc}
-                </Text>
-                <Text style={styles.date}>
-                    {moment(props.estimatedAt).locale('pt-br').format('ddd, D [de] MMMM')}
-                </Text>
-            </View>
+    const leftContent = (
+        <View style={styles.exclude}>
+            <Icon name='trash' size={20} color='#FFF' />
+            <Text style={styles.excludeText}>Excluir</Text>
         </View>
+    )
+
+    const rightContent = [
+        <TouchableOpacity
+            style={[
+                styles.exclude, {
+                    justifyContent: 'flex-start', 
+                    paddingLeft: 20
+                }
+            ]}
+            onPress={() => props.onDelete(props.id)}>
+            <Icon name='trash' size={30} color='#FFF' /> 
+        </TouchableOpacity>
+    ]
+        
+    return (
+        <Swipeable leftActionActivationDistance={200}
+            onLeftActionActivate={() => props.onDelete(props.id)}
+            leftContent={leftContent}
+            rightButtons={rightContent}>
+            <View style={styles.container}>
+                <TouchableWithoutFeedback onPress={() => props.onToggleTask(props.id)}>
+                    <View style={styles.checkContainer}>{check}</View>
+                </TouchableWithoutFeedback>
+                <View>
+                    <Text style={[styles.description, descStyle]}>
+                        {props.desc}
+                    </Text>
+                    <Text style={styles.date}>
+                        {moment(props.estimatedAt).locale('pt-br').format('ddd, D [de] MMMM')}
+                    </Text>
+                </View>
+            </View>
+        </Swipeable>    
     )    
 }
 
@@ -80,5 +107,18 @@ const styles = StyleSheet.create({
         fontFamily: commonStyles.fontFamily,
         color: commonStyles.colors.subText,
         fontSize: 12,
+    },
+    exclude: {
+        flex: 1,
+        backgroundColor: 'red',
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    excludeText: {
+        fontFamily: commonStyles.fontFamily,
+        color: '#FFF',
+        fontSize: 20,
+        margin: 10,
     }
 })
--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -111,6 +111,11 @@ export default class Agenda extends Component {
             this.filterTasks)
     }
 
+    deleteTask = id => {
+        const tasks = this.state.tasks.filter(task => task.id !== id)
+        this.setState({ tasks }, this.filterTasks)
+    }
+
     filterTasks = () => {
         let visibleTasks = null
         if (this.state.showDoneTasks) {
@@ -168,7 +173,9 @@ export default class Agenda extends Component {
                     <FlatList data={this.state.visibleTasks}
                         keyExtractor={item => `${item.id}`}
                         renderItem={({ item }) => 
-                            <Task {...item} toggleTask={this.toggleTask} />} />
+                            <Task {...item} 
+                                onToggleTask={this.toggleTask}
+                                onDelete={this.deleteTask} />} />
                 </View>
                 <ActionButton buttonColor={commonStyles.colors.today}
                     onPress={() => { this.setState({ showAddTask: true }) }} />
